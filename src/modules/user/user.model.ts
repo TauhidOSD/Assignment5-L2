9052e7cea3a52b5d51e7d8e0d2061367216a08f6
Tauhid hossen <tauhidhossen04@gmail.com>
2025-08-30 22:50:22 +0600

    user register and login succefully set data into the database test by postman and mongoDB atlus

--- src/modules/user/user.model.ts
+++ src/modules/user/user.model.ts
@@ -4,19 +4,21 @@ export interface IUser extends Document {
   name: string;
   email: string;
   password: string;
-  role: "admin" | "rider" | "driver";
+  role: string;
   isBlocked: boolean;
 }
 
-const userSchema = new Schema<IUser>(
+const userSchema: Schema<IUser> = new Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    role: { type: String, enum: ["admin", "rider", "driver"], default: "rider" },
-    isBlocked: { type: Boolean, default: false }
+    role: { type: String, enum: ["admin", "rider", "driver"], required: true },
+    isBlocked: { type: Boolean, default: false },
   },
   { timestamps: true }
 );
 
 export const User = mongoose.model<IUser>("User", userSchema);
+
+
